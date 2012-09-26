c7a032cc0c0e4aa1666412699bdc68e0db3a21a1
prabirshrestha <mail@prabir.me>
2012-09-25 23:56:27 -0400

    RegexRouter

--- src/SimpleBlog/OwinExtensions.cs
+++ src/SimpleBlog/OwinExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Owin.Extensions
+namespace SimpleBlog
 {
     using AppFunc = Func<IDictionary<string, object>, Task>;
 
