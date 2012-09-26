d687969ea3c5021d42b75a0b3de9969f46126c55
prabirshrestha <mail@prabir.me>
2012-09-26 00:28:47 -0400

    feed/article/statifiles/category/not found renderers

--- src/SimpleBlog/Renderers.cs
+++ src/SimpleBlog/Renderers.cs
@@ -2,26 +2,80 @@
 {
     using System;
     using Stream;
+    using Extensions;
     using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
 
     class Renderers
     {
         public static Func<AppFunc, AppFunc> Index()
         {
-            return next => async env =>
-                                     {
-                                         await env.GetResponseBody()
-                                             .WriteStringAsync("index");
-                                     };
+            return
+                next =>
+                async env =>
+                {
+                    await env.GetResponseBody()
+                        .WriteStringAsync("index");
+                };
         }
 
         public static Func<AppFunc, AppFunc> Feed()
         {
-            return next => async env =>
-                                     {
-                                         await env.GetResponseBody()
-                                             .WriteStringAsync("feed");
-                                     };
+            return next =>
+                   async env =>
+                   {
+                       // http://rssjs.org/
+                       var routeParameters = env.GetSimpleOwinRouteParameters();
+                       var type = routeParameters["type"];
+
+                       await env.GetResponseBody()
+                           .WriteStringAsync("feed " + type);
+                   };
+        }
+
+        public static Func<AppFunc, AppFunc> Article()
+        {
+            return
+                next =>
+                async env =>
+                {
+                    await env.GetResponseBody()
+                        .WriteStringAsync("article");
+                };
+        }
+
+        public static Func<AppFunc, AppFunc> StaticFile()
+        {
+            return
+                next =>
+                async env =>
+                {
+                    await env.GetResponseBody()
+                        .WriteStringAsync("static file");
+                };
+        }
+
+        public static Func<AppFunc, AppFunc> Category()
+        {
+            return
+                next =>
+                async env =>
+                {
+                    await env.GetResponseBody()
+                        .WriteStringAsync("category");
+                };
+        }
+
+        public static Func<AppFunc, AppFunc> NotFound()
+        {
+            return
+                next =>
+                async env =>
+                {
+                    env.SetResponseStatusCode(404);
+
+                    await env.GetResponseBody()
+                        .WriteStringAsync("not found");
+                };
         }
     }
 }
--- src/SimpleBlog/SimpleBlog.cs
+++ src/SimpleBlog/SimpleBlog.cs
@@ -2,8 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using Stream;
     using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
 
     public class SimpleBlog
@@ -15,7 +13,13 @@
             var router = new RegexRouter(app);
 
             router.Get(@"^\/()$", Renderers.Index());
-            router.Get(@"^\/()feed.xml$", Renderers.Feed());
+            router.Get(@"^\/()feed.(?<type>xml|json|js)$", Renderers.Feed());
+            router.Get(@"^\/([a-f0-9]{40})\/([a-z0-9_-]+)$", Renderers.Article());
+            router.Get(@"^\/([a-f0-9]{40})\/(.+\.[a-z]{2,4})$", Renderers.StaticFile());
+            router.Get(@"^\/()([a-z0-9_-]+)$", Renderers.Article());
+            router.Get(@"^\/()(.+\.[a-z]{2,4})$", Renderers.StaticFile());
+            router.Get(@"^\/()category\/([\%\.a-z0-9_-]+)$", Renderers.Category());
+            router.All(@"*", Renderers.NotFound());
 
             return app.ToOwinApp();
         }
