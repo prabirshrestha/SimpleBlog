8d35feafba6f8caa7551ccb68a32fc98732ec5da
prabirshrestha <mail@prabir.me>
2012-09-25 23:52:25 -0400

    add SimpleBlog app in RouteTable

---
+++ src/SimpleBlog/SimpleBlog.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleBlog
+{
+    using System.Threading.Tasks;
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+    public class SimpleBlog
+    {
+        public static AppFunc App()
+        {
+            return env => Task.FromResult(0);
+        }
+    }
+}
--- src/SimpleBlogDemo/App_Start/SimpleBlogDemoAppStart.cs
+++ src/SimpleBlogDemo/App_Start/SimpleBlogDemoAppStart.cs
@@ -3,11 +3,16 @@
 
 namespace SimpleBlogDemo.App_Start
 {
+    using System.Web.Routing;
+    using SimpleBlog;
+
     public class SimpleBlogDemoAppStart
     {
         public static void Initialize()
         {
+            var blog = SimpleBlog.App();
 
+            RouteTable.Routes.Add(new Route("{*pathInfo}", new SimpleOwinAspNetRouteHandler(blog)));
         }
     }
 }
