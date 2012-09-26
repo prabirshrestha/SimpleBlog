1a756166bf31bbcc58b6491453b556a0b4b305aa
prabirshrestha <mail@prabir.me>
2012-09-26 00:05:22 -0400

    feed.xml route

---
+++ src/SimpleBlog/Renderers.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleBlog
+{
+    using System;
+    using Stream;
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+    class Renderers
+    {
+        public static Func<AppFunc, AppFunc> Index()
+        {
+            return next => async env =>
+                                     {
+                                         await env.GetResponseBody()
+                                             .WriteStringAsync("index");
+                                     };
+        }
+
+        public static Func<AppFunc, AppFunc> Feed()
+        {
+            return next => async env =>
+                                     {
+                                         await env.GetResponseBody()
+                                             .WriteStringAsync("feed");
+                                     };
+        }
+    }
+}
--- src/SimpleBlog/SimpleBlog.cs
+++ src/SimpleBlog/SimpleBlog.cs
@@ -1,13 +1,23 @@
 ﻿namespace SimpleBlog
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Stream;
     using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
 
     public class SimpleBlog
     {
         public static AppFunc App()
         {
-            return env => Task.FromResult(0);
+            var app = new List<Func<AppFunc, AppFunc>>();
+
+            var router = new RegexRouter(app);
+
+            router.Get(@"^\/()$", Renderers.Index());
+            router.Get(@"^\/()feed.xml$", Renderers.Feed());
+
+            return app.ToOwinApp();
         }
     }
 }
