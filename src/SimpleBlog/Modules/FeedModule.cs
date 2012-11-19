b1d547f11809238a509f335e30505f8253a44ecc
Prabir Shrestha <mail@prabir.me>
2012-11-19 00:50:38 -0500

    Feed Module

---
+++ src/SimpleBlog/Modules/FeedModule.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleBlog.Modules
+{
+    using Nancy;
+
+    public class FeedModule : NancyModule
+    {
+        public FeedModule()
+        {
+            Get["/feed.{format}"] =
+                x => "feed " + x.format;
+        }
+    }
+}
