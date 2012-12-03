f02b92bc64b674a0a87a4ab8b299dc7d960b67b8
prabirshrestha <mail@prabir.me>
2012-12-03 02:17:04 -0500

    use service stack serializer and optimize cassette

--- src/SimpleBlog/Bootstrapper.cs
+++ src/SimpleBlog/Bootstrapper.cs
@@ -1,15 +1,37 @@
 ﻿namespace SimpleBlog
 {
     using Nancy;
+    using Nancy.Bootstrapper;
     using Nancy.Conventions;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            ServiceStack.Text.JsConfig.EmitCamelCaseNames = true;
+
+#if !DEBUG
+            Cassette.Nancy.CassetteNancyStartup.OptimizeOutput = true;
+#endif
+
+        }
         protected override void ConfigureConventions(NancyConventions nancyConventions)
         {
             base.ConfigureConventions(nancyConventions);
             nancyConventions.StaticContentsConventions.Clear();
             nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/", "public"));
+        }
+
+        protected override NancyInternalConfiguration InternalConfiguration
+        {
+            get
+            {
+                return NancyInternalConfiguration.WithOverrides(
+                    x => {
+                        x.Serializers.Insert(0, typeof(Nancy.Serializers.Json.ServiceStack.ServiceStackJsonSerializer));
+                    });
+            }
         }
     }
 }
